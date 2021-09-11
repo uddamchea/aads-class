bf3c00a9d4d7cf9625235752c67531c3412d3c90
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-09-11 14:51:16 -0500

    Almost Done

--- src/projects/classy/classifier.py
+++ src/projects/classy/classifier.py
@@ -7,7 +7,50 @@ from typing import Dict, List
 
 
 def classify(people: dict) -> list[str]:
-    pass
+    for key, value in people.items():
+        cleanValue = value.split("-")
+        cleanValue = cleanValue[::-1]
+        people[key] = cleanValue
+    #print(people)
+
+    cleanValueList = list(people.values())
+    maxValueList=[]
+    for x in range(0, len(cleanValueList)):
+        maxValueList.append(len(cleanValueList[x]))
+        maxValue = max(maxValueList)
+    #print(max(maxValueList))
+
+    for key, value in people.items():
+        for y in range(maxValue - len(value)):
+            value.insert(len(value), "middle")
+        people[key] = value
+    #print(people)
+
+    for key, value in people.items():
+        for z in range(len(value)):
+            if value[z] == "lower":
+                value[z] = 1
+            if value[z] == "middle":
+                value[z] = 2
+            if value[z] == "upper":
+                value[z] = 3
+        people[key] = value
+    #print(people)
+
+    for key, value in people.items():
+        for e in range(len(value)):
+            strings = [str(integer) for integer in value]
+            a_string = "".join(strings)
+        people[key] = a_string
+    #print(people)
+
+    resultDict = {k: v for k,v in sorted(people.items(), key= lambda v: v[1], reverse=True)}
+    #return resultDict
+    #print(resultDict)
+
+    answers = list(resultDict.keys())
+    #print(answers)
+    return answers
 
 def read_file(filename: str) -> dict[str, str]:
     data = open(filename, "r")
@@ -17,7 +60,7 @@ def read_file(filename: str) -> dict[str, str]:
         splitData.pop()
         cleanData = [s.replace(":", "") for s in splitData]
         dict[cleanData[0]] = "".join(cleanData[1:])
-    #print(dict)
+    return dict
 
 def main():
     """Entry point"""
