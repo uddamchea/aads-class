9f2805e8e0c9829995f219e230127a276684b9be
Ratanak Chea <cheara01@luther.edu>
2021-09-12 16:08:17 -0500

    Started

--- src/exercises/hashing/hashing.py
+++ src/exercises/hashing/hashing.py
@@ -4,25 +4,37 @@
 
 def hash_remainder(key: int, size: int) -> int:
     """Find hash using remainder"""
-    raise NotImplementedError
+    inputKey = int(input("Pick a key: "))
+    inputSize = int(input("Enter table size: "))
+    dict = {}
+    for i in dict.items():
+        slotLabels = [x for x in range(0, inputSize+1)]
+        dict[inputKey] = slotLabels
+    print(dict)
+
+    # raise NotImplementedError
 
 
 def hash_mid_sqr(key: int, size: int) -> int:
     """Find hash using mid-square method"""
-    raise NotImplementedError
+    #raise NotImplementedError
+    pass
 
 
 def hash_folding(key: int, size: int) -> int:
     """Find hash using folding method"""
-    raise NotImplementedError
+    #raise NotImplementedError
+    pass
 
 
 def hash_str(key: str, size: int) -> int:
     """Find string hash using simple sum-of-values method"""
-    raise NotImplementedError
+    #raise NotImplementedError
+    pass
 
 
 def hash_str_weighted(key: str, size: int) -> int:
     """Find string hash using character positions as weights"""
-    raise NotImplementedError
+    #raise NotImplementedError
+    pass
 
