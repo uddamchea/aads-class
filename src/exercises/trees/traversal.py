1558dce53deee7d01c15c5630443f594e60ac9a1
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-09-21 12:46:50 -0500

    Almost Done

--- src/exercises/trees/traversal.py
+++ src/exercises/trees/traversal.py
@@ -3,10 +3,36 @@
 
 
 def get_preorder(inorder: str, postorder: str) -> str:
-    """Return pre-order traversal of a tree based on its in-order and post-order traversals"""
-    raise NotImplementedError
+    inorder = list(str(inorder))
+    postorder = list(str(postorder))
 
+    emptyStr = ""
 
+    root = postorder.pop()
+    rootIndex = inorder.index(root)
+    rightSubtree = inorder[rootIndex+1:]
+    leftSubtree = inorder[:rootIndex]
+
+    if not leftSubtree and not rightSubtree:
+        result = emptyStr
+        return result
+
+    elif leftSubtree:
+        inorder = leftSubtree
+        result = emptyStr + root
+        get_preorder(inorder, postorder)
+
+    else: 
+        inorder = rightSubtree
+        result = emptyStr + root
+        get_preorder(inorder, postorder)
+    # elif \
+    #     leftSubtree:
+    #     inorder = leftSubtree
+    #     get_preorder(inorder, postorder)
+    # else:
+    #     return tree
+        
 def main():
     """This is the main function"""
     print(get_preorder(inorder="UOMELBARTKGSNI", postorder="UMELABORSGNIKT"))
