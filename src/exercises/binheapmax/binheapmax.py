30ce878172b950ef93c013f17140aea74b85283d
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-10-06 11:25:04 -0500

    Nearly Done

--- src/exercises/binheapmax/binheapmax.py
+++ src/exercises/binheapmax/binheapmax.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 """Binary Heap implementation"""
 
-<<<<<<< HEAD
-=======
-
-from typing import Any
-
-
->>>>>>> 82e5a153f468d411e1abd2bd3d64cc853d219cfc
 class BinaryHeapMax:
     """Heap class implementation"""
 
@@ -18,21 +11,15 @@ class BinaryHeapMax:
 
     def _perc_up(self, cur_idx: int) -> None:
         """Move a node up"""
-<<<<<<< HEAD
         while (cur_idx - 1) // 2 >= 0:
             parent_idx = (cur_idx - 1) // 2
             if self._heap[cur_idx] > self._heap[parent_idx]:
                 self._heap[cur_idx], self._heap[parent_idx] = self._heap[parent_idx], self._heap[cur_idx]
             parent_idx = cur_idx    
             # cur_idx = parent_idx
-=======
-        # TODO: Implement this function
-        ...
->>>>>>> 82e5a153f468d411e1abd2bd3d64cc853d219cfc
 
     def _perc_down(self, cur_idx: int) -> None:
         """Move a node down"""
-<<<<<<< HEAD
         while 2 * cur_idx + 2 < len(self._heap):
             max_child_idx = self.get_max_child(cur_idx)
             if self._heap[cur_idx] < self._heap[max_child_idx]:
@@ -41,10 +28,6 @@ class BinaryHeapMax:
                 return
             max_child_idx = cur_idx
             # cur_idx = max_child_idx
-=======
-        # TODO: Implement this function
-        ...
->>>>>>> 82e5a153f468d411e1abd2bd3d64cc853d219cfc
 
     def add(self, item: Any) -> None:
         """Add a new item"""
@@ -58,7 +41,6 @@ class BinaryHeapMax:
 
     def heapify(self, not_a_heap: list) -> None:
         """Turn a list into a heap"""
-<<<<<<< HEAD
         self._heap = not_a_heap[:]
         cur_idx = len(self._heap) // 2 - 1
         while cur_idx >= 0:
@@ -72,15 +54,6 @@ class BinaryHeapMax:
         if self._heap[2 * parent_idx + 1] < self._heap[2 * parent_idx + 2]:
             return 2 * parent_idx + 2
         return 2 * parent_idx + 2
-=======
-        # TODO: Implement this function
-        ...
-
-    def _get_max_child(self, parent_idx: int) -> int:
-        """Get index of the greater child"""
-        # TODO: Implement this function
-        ...
->>>>>>> 82e5a153f468d411e1abd2bd3d64cc853d219cfc
 
     def __len__(self) -> int:
         """Get heap size"""
