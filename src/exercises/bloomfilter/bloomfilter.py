9f1f327d01e8b30ead102710f20851f4c547cac6
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-10-11 11:32:04 -0500

    Started

--- src/exercises/bloomfilter/bloomfilter.py
+++ src/exercises/bloomfilter/bloomfilter.py
@@ -12,23 +12,24 @@ from zlib import crc32
 class BloomFilter:
     def __init__(self, size: int = 11, k: int = 3) -> None:
         """Initialize the filter"""
-        # TODO: Implement this method
-        ...
+        self.k = k
+        self._filter = [False] * size
 
     def hash(self, word: str) -> tuple[int, ...]:
         """Return a tuple of k indices"""
-        # TODO: Implement this method
-        ...
+        return tuple((crc32(bytes(f"{word}{i}", "utf8")) % len(self._filter) for i in range(self.k)))
 
     def add(self, word: str) -> None:
         """Add a dictionary word to the filter"""
-        # TODO: Implement this method
-        ...
+        #call _hash
+        #hash the word with k=3 (use loop) to get a tuple of 3 elements
+        #call __contains__
+        #add into the dict if the an element of the tuple is available
 
     def __contains__(self, word: str) -> bool:
         """Check if a word in in the filter"""
-        # TODO: Implement this method
-        ...
+        return all([self._filter[i] for i in self._hash(word)])
+
 
     def __str__(self) -> str:
         """Return string representation of the filter"""
