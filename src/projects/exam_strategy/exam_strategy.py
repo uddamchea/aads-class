6f7a13957c39e450ebec3e6f42a6db5da07c07e2
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-11-05 19:51:20 -0500

    Completed

--- src/projects/exam_strategy/exam_strategy.py
+++ src/projects/exam_strategy/exam_strategy.py
@@ -18,7 +18,52 @@ def knapsack(capacity: int, items: List[Item]) -> List[int]:
     This function is optional but highly recommended.
     Use of the named tuple Item is optional but encouraged.
     """
-    pass
+    value = []
+    weight = []
+
+    for i in items:
+        value.append(i[0])
+        weight.append(i[1])
+
+    Item = []
+    numberofQuestion = weight[0]
+    
+    Item = [[0 for i in range(capacity+1)] for i in range(numberofQuestion+1)]
+
+    value.pop(0)
+    weight.pop(0)
+
+    for i in range(numberofQuestion+1): 
+        for j in range(capacity + 1):
+
+            # m[0,w] = 0
+            if i == 0 or j == 0:
+                Item[i][j] = 0
+
+            # m[i,w] = m[i-1,w] if wi > w
+            elif weight[i-1] > j:
+                Item[i][j] = Item[i-1][j]
+
+            # m[i,w] = max(m[i-1,w], m[i-1,w-wi]+vi) if wi <= w
+            else:
+                Item[i][j] = max(Item[i-1][j], Item[i-1][j-weight[i-1]] + value[i-1])
+    
+    indeces = Item[numberofQuestion][capacity]
+    currentCapacity = capacity
+    result = []
+
+    for i in range(numberofQuestion, 0, -1):
+
+        if Item[numberofQuestion][capacity] != Item[i-1][currentCapacity]:
+            result.append([i-1])
+            result.append(weight[i - 1]) 
+            Item[numberofQuestion][capacity] -= value[i - 1] 
+            currentCapacity -= weight[i - 1]
+            
+    final=[j for i in result[::2] for j in i]
+    final.reverse()
+    return [final, indeces]
+
 
 
 def pick_questions_to_answer(filename: str) -> Tuple[List[int], int]:
@@ -28,8 +73,14 @@ def pick_questions_to_answer(filename: str) -> Tuple[List[int], int]:
     This function takes file name as an argument.
     The function returns a tuple of two items: the list of chosen indices and total point value of all selected questions.
     """
-    raise NotImplementedError
-
+    chosenIndices = []
+    file = open(filename)
+    for line in file:
+        line = line.split()
+        if line:          
+            line = [int(float(i)) for i in line]
+            chosenIndices.append(line)
+    return (knapsack(chosenIndices[0][0], chosenIndices))
 
 def main():
     """Entry point"""
