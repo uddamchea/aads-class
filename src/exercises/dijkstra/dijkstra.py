71b8535fd1d17ebe47b3d10a5e801e21f954e9a5
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-11-11 14:50:24 -0600

    Started

--- src/exercises/dijkstra/dijkstra.py
+++ src/exercises/dijkstra/dijkstra.py
@@ -8,15 +8,25 @@ import toml
 
 def read_toml(filename: str) -> Graph:
     """Read TOML config file"""
-    pass
+    file = toml.load(filename)
+    data = dict()
+    graph = Graph()
+    # n, g =  dict(), Graph()
+    for i in file['routers']:
+        data[i['address']] = i['name']
+    
+    for j in file['routers']:
+        for k in j['neighbors']: graph.add_edge(j['name'], data[k['address']], k['cost'])
+    return graph
 
 def find_path(g: Graph, start: str) -> None:
     """Use Dijkstra's algorithm to find the shortest path from *start* to other vertices"""
-    pass
+    print(g.dijkstra(g.get_vertex(start)))
 
 
 def main():
-    pass
+    graph = read_toml("data/exercises/dijkstra/network.toml")
+    find_path(graph, 'v')
 
 
 if __name__ == "__main__":
